5c4e1e6ef38eddc73c4305a407e7d761bd206581
Nodirbek Choriyev <167026288+nchoriyev@users.noreply.github.com>
2024-06-13 23:32:09 +0500

    Update CloneRangeWithClass.py

--- CloneRangeWithClass.py
+++ CloneRangeWithClass.py
@@ -1,37 +1,28 @@
-# range(x) => (0, x -1) step = 1
-# range(x, y) => (x, y-1) step = 1
-# range(x, y, z) => (x, y - 1) step = z
-import time
-
-
 class Range:
     def __init__(self, start, stop=None, step=1):
         if stop is None:
-            self.stop = start   # stop = 10
-            self.start = -step  # start = -1
-            self.step = step    # step = 1
+            self.stop = start
+            self.start = -step
+            self.step = step
         else:
             self.start = start - step
             self.stop = stop
             self.step = step
 
     def __iter__(self):
-        # print("call iter")
         return self
 
     def __next__(self):
-        # if self.step > 0:
-        # print("next iter")
         if self.start > self.stop and self.step > 0:
             raise StopIteration
 
-        elif self.start > self.stop and self.step < 0:
+        elif self.start > self.stop and self.step < 0: # (-5, -27, -2)
             self.start += self.step
             if self.stop >= self.start:
                 raise StopIteration
             return self.start
 
-        elif self.start < self.stop and self.step > 0:
+        elif self.start < self.stop and self.step > 0: # (1, 78, 3)
             self.start += self.step
             if self.start >= self.stop:
                 raise StopIteration
@@ -39,5 +30,5 @@ class Range:
 
 
 
-for i in Range(2, 25, 2.5):
+for i in Range(-5,-28,-5):
     print(i)
